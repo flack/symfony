5ff67d49ba67bba6d6f24289589aa38f0c5e94b6
flack <flack@contentcontrol-berlin.de>
2021-09-08 18:47:17 +0200

    Small optimization/readability improvement

--- src/Symfony/Component/Mime/Email.php
+++ src/Symfony/Component/Mime/Email.php
@@ -471,7 +471,7 @@ class Email extends Message
         }
 
         $attachmentParts = $inlineParts = [];
-        foreach ($this->attachments as $attachment) {
+        foreach ($this->attachments as $i => $attachment) {
             foreach ($names as $name) {
                 if (isset($attachment['part'])) {
                     continue;
@@ -479,13 +479,11 @@ class Email extends Message
                 if ($name !== $attachment['name']) {
                     continue;
                 }
-                if (isset($inlineParts[$name])) {
-                    continue 2;
-                }
                 $attachment['inline'] = true;
-                $inlineParts[$name] = $part = $this->createDataPart($attachment);
+                $inlineParts[] = $part = $this->createDataPart($attachment);
                 $html = str_replace('cid:'.$name, 'cid:'.$part->getContentId(), $html);
                 $part->setName($part->getContentId());
+                unset($names[$i]);
                 continue 2;
             }
             $attachmentParts[] = $this->createDataPart($attachment);
@@ -494,7 +492,7 @@ class Email extends Message
             $htmlPart = new TextPart($html, $this->htmlCharset, 'html');
         }
 
-        return [$htmlPart, $attachmentParts, array_values($inlineParts)];
+        return [$htmlPart, $attachmentParts, $inlineParts];
     }
 
     private function createDataPart(array $attachment): DataPart
